886ed40289902b0636f9973f978de4c5a407228b
Łukasz Hejnak (LeHack) <lehack-ghub@lehack.pl>
2017-06-07 19:50:41 +0200

    Added loops and simple conditionals. Still not all rules are correct.

--- src/ply/lex_example.py
+++ src/ply/lex_example.py
@@ -4,11 +4,73 @@ import re
 from ply.lex import TOKEN
 
 DEBUG_MODE = False
+variables = {}
+
+
+class DelayedActions:
+    action = None
+    params = None
+
+    def __init__(self, action=None, params=None):
+        self.action = action
+        self.params = params
+
+    def execute(self):
+        result = None
+        if self.action == 'print':
+            print(' '.join(str(DelayedActions.resolve(x)) for x in list(self.params)))
+        elif self.action == 'assign':
+            variables[self.params[0]] = DelayedActions.resolve(self.params[1])
+        elif self.action == 'get':
+            result = variables.get(self.params[0], 0)
+        elif self.action == 'loop':
+            for i in self.params[1]:
+                variables[self.params[0]] = i
+                self.params[2].execute()
+        elif self.action == 'condition':
+            if DelayedActions.resolve(self.params[0]):
+                DelayedActions.resolve(self.params[1])
+            elif len(self.params) > 2:
+                DelayedActions.resolve(self.params[2])
+        elif self.action == 'binop':
+            a = DelayedActions.resolve(self.params[0])
+            b = DelayedActions.resolve(self.params[2])
+            result = {
+                '+':  lambda a, b: a + b,
+                '-':  lambda a, b: a - b,
+                '*':  lambda a, b: a * b,
+                '/':  lambda a, b: a / b,
+                '%':  lambda a, b: a % b,
+                '**': lambda a, b: a ** b,
+                '>':  lambda a, b: (a > b),
+                '>=': lambda a, b: (a >= b),
+                '<':  lambda a, b: (a < b),
+                '<=': lambda a, b: (a <= b),
+                '==': lambda a, b: (a == b),
+                '!=': lambda a, b: (a != b),
+            }[self.params[1]](a, b)
+
+        debug('Resolving', str(self), result)
+        return result
+
+    def __str__(self):
+        return '[DelAct] %s %s' % (self.action, ';'.join(str(x) for x in self.params))
+
+    @staticmethod
+    def isADelayedAction(x):
+        return (x and type(x) == DelayedActions)
+
+    @staticmethod
+    def resolve(x):
+        if not DelayedActions.isADelayedAction(x):
+            return x
+        else:
+            return x.execute()
 
 
 def debug(*params):
     if DEBUG_MODE:
-        print("[DBG] %s" % (', '.join(str(x) for x in params),))
+        print("[DBG] %s" % (' : '.join(str(x) for x in params),))
 
 
 states = (
@@ -19,7 +81,6 @@ states = (
 tokens = (
     'NUMBER',
     'ID',
-    'SPECIAL',
     'SEMI',
     'STRING',
 )
@@ -28,13 +89,9 @@ reserved = {
     'print': 'PRINT',
     'range': 'RANGE',
     'if': 'IF',
-    'then': 'THEN',
     'else': 'ELSE',
-    'while': 'WHILE',
     'for': 'FOR',
     'in': 'IN',
-    'break': 'BREAK',
-    'continue': 'CONTINUE',
 }
 
 # List of single character literals
@@ -43,16 +100,12 @@ specials_sc = {
     '-':    'REM',
     '*':    'MUL',
     '/':    'DIV',
+    '%':    'MOD',
     '=':    'ASSIGN',
     '<':    'LT',
     '>':    'GT',
     '(':    'LPAREN',
     ')':    'RPAREN',
-    '[':    'LBRACKET',
-    ']':    'RBRACKET',
-    '{':    'LBRACE',
-    '}':    'RBRACE',
-    '%':    'PERCENT',
     ',':    'COMMA',
     ':':    'COLON',
 }
@@ -67,8 +120,8 @@ specials_mc = {
 }
 
 precedence = (
-    ('nonassoc', 'ELSE'),
     ('left', 'LT', 'LE', 'GT', 'GE', 'EQ', 'NE'),
+    ('left', 'MOD'),
     ('left', 'ADD', 'REM'),
     ('left', 'MUL', 'DIV'),
     ('left', 'POW'),
@@ -114,6 +167,7 @@ def t_STRING(t):
     r'[\"\']'
     t.lexer.begin('string')
     t.lexer.str_start = t.lexer.lexpos
+    t.lexer.str_marker = t.value
 
 
 def t_string_chars(t):
@@ -128,10 +182,12 @@ def t_string_newline(t):
 
 def t_string_end(t):
     r'[\"\']'
-    t.type = 'STRING'
-    t.value = t.lexer.lexdata[t.lexer.str_start:t.lexer.lexpos - 1]
-    t.lexer.begin('INITIAL')
-    return t
+
+    if t.lexer.str_marker == t.value:
+        t.type = 'STRING'
+        t.value = t.lexer.lexdata[t.lexer.str_start:t.lexer.lexpos - 1]
+        t.lexer.begin('INITIAL')
+        return t
 
 
 @TOKEN(specials_mc_re)
@@ -158,7 +214,6 @@ def t_error(t):
 
 # Build the lexer
 lexer = lex.lex()
-variables = {}
 
 
 def p_program(p):
@@ -166,7 +221,7 @@ def p_program(p):
                | empty
     '''
     if len(p) > 2:
-        p[0] = p[2]
+        p[0] = DelayedActions.resolve(p[2])
 
 
 def p_statement_none(p):
@@ -179,11 +234,22 @@ def p_statement_expr(p):
     p[0] = p[1]
 
 
+def p_statement_for(p):
+    'statement : FOR ID IN statement COLON statement SEMI'
+    debug('FOR', p[1:])
+    p[0] = DelayedActions(action='loop', params=[p[2], p[4], p[6]])
+
+
 def p_statement_print(p):
     'statement : PRINT LPAREN expr_list RPAREN'
     debug('PRINT', p[3])
-    print(' '.join(str(x) for x in p[3]))
-    p[0] = None
+    p[0] = DelayedActions(action='print', params=p[3])
+
+
+def p_statement_range(p):
+    'statement : RANGE LPAREN expr_list RPAREN'
+    debug('RANGE', p[3])
+    p[0] = list(range(p[3][0], p[3][1]))
 
 
 def p_expression_list(p):
@@ -200,14 +266,32 @@ def p_expression_list(p):
 def p_statement_assign(p):
     'statement : ID ASSIGN expression SEMI'
     debug('ASSIGN', p[1], p[3])
-    variables[p[1]] = p[3]
-    p[0] = p[3]
+    p[0] = DelayedActions(action='assign', params=[p[1], p[3]])
+
+
+def p_statement_cond_postfix_else(p):
+    'statement : statement IF expression ELSE statement SEMI'
+    debug("PSTFX IF-ELSE", p[1:])
+    p[0] = DelayedActions(action='condition', params=[p[3], p[1], p[5]])
+
+
+def p_statement_cond_postfix(p):
+    'statement : statement IF expression SEMI'
+    debug("PSTFX IF", p[1:])
+    p[0] = DelayedActions(action='condition', params=[p[3], p[1]])
+
+
+def p_statement_cond(p):
+    'statement : IF expression COLON statement SEMI'
+    debug("IF", p[1:])
+    p[0] = DelayedActions(action='condition', params=[p[2], p[4]])
 
 
 def p_expression_var(p):
     'expression : ID'
     debug('VAR', p[1])
-    p[0] = variables.get(p[1], 0)
+    # p[0] = variables.get(p[1], 0)
+    p[0] = DelayedActions(action='get', params=[p[1]])
 
 
 def p_expression_num(p):
@@ -228,40 +312,22 @@ def p_expression_uminus(p):
     p[0] = -p[2]
 
 
-def p_expression_count(p):
+def p_expression_binop(p):
     '''expression  : expression ADD expression
                    | expression REM expression
                    | expression MUL expression
                    | expression DIV expression
+                   | expression MOD expression
                    | expression POW expression
-    '''
-    debug('COUNT', p[1], p[2], p[3])
-    p[0] = {
-        '+':  lambda x: x[1] + x[3],
-        '-':  lambda x: x[1] - x[3],
-        '*':  lambda x: x[1] * x[3],
-        '/':  lambda x: x[1] / x[3],
-        '**': lambda x: x[1] ** x[3],
-    }[p[2]](p)
-
-
-def p_expression_logic(p):
-    '''expression  : expression GT expression
+                   | expression GT expression
                    | expression GE expression
                    | expression LT expression
                    | expression LE expression
                    | expression EQ expression
                    | expression NE expression
     '''
-    debug('LOGIC', p[1], p[2], p[3])
-    p[0] = {
-        '>':  lambda x: x[1] > x[3],
-        '>=': lambda x: x[1] >= x[3],
-        '<':  lambda x: x[1] < x[3],
-        '<=': lambda x: x[1] <= x[3],
-        '==': lambda x: x[1] == x[3],
-        '!=': lambda x: x[1] != x[3],
-    }[p[2]](p)
+    debug('COUNT', p[1:])
+    p[0] = DelayedActions(action='binop', params=p[1:])
 
 
 def p_expression_parens(p):
@@ -281,25 +347,34 @@ def p_empty(p):
 
 
 # Build the parser
-parser = yacc.yacc(debug=True)
+parser = yacc.yacc()
 
 # Test it out
-f = open('input_code.py', 'r')
-data = f.read()
-f.close()
+# f = open('input_code.py', 'r')
+# data = f.read()
+# f.close()
 
 data = '''
 x = 2 ** 8 + (-1 - 6) * 8
 x = x + 5
-t1 = x > 5
-print(x + 5)
-print(x, 5, x+5, 100)
+t1 = x < 5
+print(10, x + 5)
+print('x =', x)
+print('x + 5 =', x + 5)
+print('x % 100 =', x % 100)
+print('x == 205 is', x == 205, '; x != 210 is', x != 210,'; x < 5 is', t1)
 print("Test", 'def', 1)
+print("Here you see x") if x > 10
+print("Here you don't") if x < 10
+for i in range(1, 10): print("i =", i * 2)
+x + 2
 '''
-# if t1:
-#     print(1)
+
+# if t1: print(1)
+# if x < 10: x = 11
+# x = 15 if x < 10 else print("Test 2")
 
 
 # Now parse the whole thing
 print('Statement value:', parser.parse(data))
-print(repr(variables))
+print('Variables:', repr(variables))
